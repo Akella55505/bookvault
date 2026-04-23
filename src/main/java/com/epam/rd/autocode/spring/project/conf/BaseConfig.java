258a32ba3d3a7626aaf9ba317284cd34592f27e1
Akella <leosaz0617@gmail.com>
2026-04-23 16:03:22 +0300

    Add base bean configuration

--- src/main/java/com/epam/rd/autocode/spring/project/conf/BaseConfig.java
+++ src/main/java/com/epam/rd/autocode/spring/project/conf/BaseConfig.java
@@ -1,5 +1,41 @@
 package com.epam.rd.autocode.spring.project.conf;
 
-public class BaseConfig{
-    // Place your code here
+import com.epam.rd.autocode.spring.project.converter.StringToEnumConverterFactory;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class BaseConfig implements WebMvcConfigurer {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverterFactory(new StringToEnumConverterFactory());
+    }
 }
