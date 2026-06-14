6a976785bd2210734a8eae9fe72f65303a35178f
Akella <leosaz0617@gmail.com>
2026-06-14 14:36:19 +0300

    Fix SameSite attribute strictness

--- src/main/java/com/epam/rd/autocode/spring/project/service/CookieService.java
+++ src/main/java/com/epam/rd/autocode/spring/project/service/CookieService.java
@@ -8,7 +8,7 @@ public class CookieService {
     public Cookie createCookie(String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
-        cookie.setAttribute("SameSite", "Strict");
+        cookie.setAttribute("SameSite", "Lax");
         cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge(maxAge);
